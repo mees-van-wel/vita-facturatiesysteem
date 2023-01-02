12c1f179d3842618f533cef473302830a88661bc
Mees van Wel <mees.vanwel@hexa-it.nl>
2023-01-02 14:33:12 +0100

    Updated libs

--- pages/api/authorize.ts
+++ pages/api/authorize.ts
@@ -25,7 +25,7 @@ export default async function handler(
 
   // console.log(usera);
 
-  const user = await prisma.user.findUniqueOrThrow({ where: { email } });
+  const user = await prisma.user.findUnique({ where: { email } });
 
   if (!!user && (await argon2.verify(user.password, password)))
     return res.status(200).json(user);
