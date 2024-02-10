ef918396d02e8ea83838091105ec4d5bdc78c357
Mees van Wel <mees@hexa-it.nl>
2024-02-10 19:44:05 +0100

    Added base s3 client

---
+++ src/utilities/s3Client.ts
@@ -0,0 +1,11 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+  region: "eu2",
+  forcePathStyle: true,
+  endpoint: "https://eu2.contabostorage.com",
+  credentials: {
+    accessKeyId: process.env.CONTABO_STORAGE_ACCESS_KEY!,
+    secretAccessKey: process.env.CONTABO_STORAGE_SECRET_KEY!,
+  },
+});
