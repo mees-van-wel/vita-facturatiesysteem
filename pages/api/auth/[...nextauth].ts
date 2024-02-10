e4172575cceafed8c7bfe5e7fbeafd0b0a155098
Mees van Wel <mees@hexa-it.nl>
2024-02-10 23:01:38 +0100

    inlined auth logic

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma.lib";
+import * as argon2 from "argon2";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -10,13 +12,27 @@ export const authOptions: NextAuthOptions = {
         email: { label: "E-mail", type: "email", required: true },
         password: { label: "Wachtwoord", type: "password", required: true },
       },
+      // @ts-ignore
       authorize: async (credentials) => {
-        const res = await axios.post(
-          `${process.env.NEXTAUTH_URL}/api/authorize`,
-          credentials
-        );
+        // const res = await axios.post(
+        //   `${process.env.NEXTAUTH_URL}/api/authorize`,
+        //   credentials
+        // );
 
-        if (res.status === 200 && !!res.data) return res.data;
+        if (!credentials) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (
+          user &&
+          !user.deactivated &&
+          (await argon2.verify(user.password, credentials.password))
+        )
+          return user;
+
+        // if (res.status === 200 && !!res.data) return res.data;
 
         return null;
       },
